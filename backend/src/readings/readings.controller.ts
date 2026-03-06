50791ca594db7451fae72a1b5e3a62ee5f21d782
Jose Manuel Ferrer Vidal <jjferrer0202@gmail.com>
2026-03-06 11:41:20 -0500

    Fix Prisma OpenSSL issue in backend Docker image

---
+++ backend/src/readings/readings.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ReadingsService } from './readings.service';
+
+@Controller('readings')
+export class ReadingsController {
+  constructor(private readonly readingsService: ReadingsService) {}
+
+  @Post()
+  createReading(@Body() body: { rawCm: number }) {
+    return this.readingsService.createReading(body.rawCm);
+  }
+}
